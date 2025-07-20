4ef49374839f7d6b4721ee9159c047c28654b770
FlameOff <jgmppacheco@gmail.com>
2025-07-20 17:25:04 -0300

    fix:CategoriaController.php

--- app/Controller/CategoriaController.php
+++ app/Controller/CategoriaController.php
@@ -1,28 +1,29 @@
 <?php
-
 namespace App\Controller;
 
-use App\DAO\CategoriaDAO;
-use App\Model\Categoria;
+use App\Service\CategoriaService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class CategoriaController {
+    private CategoriaService $service;
+
+    public function __construct(CategoriaService $service) {
+        $this->service = $service;
+    }
 
     public function listar(Request $request, Response $response): Response {
-        $dao = new CategoriaDAO();
-        $categoria = $dao->listar();
-        $response->getBody()->write(json_encode($categoria));
+        $categorias = $this->service->listar();
+        $response->getBody()->write(json_encode($categorias));
         return $response->withHeader('Content-Type', 'application/json');
     }
 
     public function buscarPorId(Request $request, Response $response, array $args): Response {
         $id = (int) $args['id'];
-        $dao = new CategoriaDAO();
-        $categoria = $dao->listarPorId($id);
+        $categoria = $this->service->listarPorId($id);
 
         if (!$categoria) {
-            $response->getBody()->write(json_encode(['erro' => 'Categoria não encontrado']));
+            $response->getBody()->write(json_encode(['erro' => 'Categoria não encontrada']));
             return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
         }
 
@@ -32,13 +33,10 @@ class CategoriaController {
 
     public function criar(Request $request, Response $response): Response {
         $data = $request->getParsedBody();
-        $categoria = new Categoria($data);
-
-        $dao = new CategoriaDAO();
-        $sucesso = $dao->inserir($categoria);
+        $sucesso = $this->service->criar($data);
 
         $status = $sucesso ? 201 : 500;
-        $mensagem = $sucesso ? ['mensagem' => 'Categoria criado com sucesso'] : ['erro' => 'Erro ao criar categoria'];
+        $mensagem = $sucesso ? ['mensagem' => 'Categoria criada com sucesso'] : ['erro' => 'Erro ao criar categoria'];
 
         $response->getBody()->write(json_encode($mensagem));
         return $response->withStatus($status)->withHeader('Content-Type', 'application/json');
@@ -47,14 +45,9 @@ class CategoriaController {
     public function atualizar(Request $request, Response $response, array $args): Response {
         $id = (int) $args['id'];
         $data = $request->getParsedBody();
+        $sucesso = $this->service->atualizar($id, $data);
 
-        $categoria = new Categoria($data);
-        $categoria->setId($id);
-
-        $dao = new CategoriaDAO();
-        $sucesso = $dao->atualizar($categoria);
-
-        $mensagem = $sucesso ? ['mensagem' => 'Categoria atualizado com sucesso'] : ['erro' => 'Erro ao atualizar categoria'];
+        $mensagem = $sucesso ? ['mensagem' => 'Categoria atualizada com sucesso'] : ['erro' => 'Erro ao atualizar categoria'];
         $status = $sucesso ? 200 : 500;
 
         $response->getBody()->write(json_encode($mensagem));
@@ -63,11 +56,9 @@ class CategoriaController {
 
     public function deletar(Request $request, Response $response, array $args): Response {
         $id = (int) $args['id'];
+        $sucesso = $this->service->deletar($id);
 
-        $dao = new CategoriaDAO();
-        $sucesso = $dao->deletar($id);
-
-        $mensagem = $sucesso ? ['mensagem' => 'Categoria deletado com sucesso'] : ['erro' => 'Erro ao deletar categoria'];
+        $mensagem = $sucesso ? ['mensagem' => 'Categoria deletada com sucesso'] : ['erro' => 'Erro ao deletar categoria'];
         $status = $sucesso ? 200 : 500;
 
         $response->getBody()->write(json_encode($mensagem));
