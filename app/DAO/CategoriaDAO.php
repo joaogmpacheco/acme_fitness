8db6b06a5e43dd29cc431651f539087e7368231a
FlameOff <jgmppacheco@gmail.com>
2025-07-21 17:25:32 -0300

    refactor:app/DAO/CategoriaDAO.php

--- app/DAO/CategoriaDAO.php
+++ app/DAO/CategoriaDAO.php
@@ -1,17 +1,15 @@
 <?php
 
 namespace App\DAO;
-use App\Database\Connection;
 use App\Model\Categoria;
 use PDO;
 
 class CategoriaDAO {
     private PDO $pdo;
 
-    public function __construct() {
-        $this->pdo = Connection::get();
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
     }
-
     public function listar(): array {
         $stmt = $this->pdo->query("SELECT * FROM categoria");
         $categoria = [];
@@ -48,7 +46,7 @@ class CategoriaDAO {
         return $stmt->execute([
             ':nome' => $categoria->getNome(),
             ':descricao' => $categoria->getDescricao(),
-
+            ':id' => $categoria->getId(), // <-- necessário
         ]);
     }
 
