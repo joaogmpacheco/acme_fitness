52035a01dff1307c2c0e26a83d34db679b6f04c4
FlameOff <jgmppacheco@gmail.com>
2025-07-21 17:18:01 -0300

    refactor:app/routes/variacaoProduto.php

--- app/routes/variacaoProduto.php
+++ app/routes/variacaoProduto.php
@@ -1,11 +1,15 @@
 <?php
 
+use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 use App\Controller\VariacaoProdutoController;
 
-$app->group('/variacaoProduto', function () {
-    $this->get('', VariacaoProdutoController::class . ':listar'); // GET /variacaoProduto
-    $this->get('/{id}', VariacaoProdutoController::class . ':listarPorId'); // GET /variacaoProduto/{id}
-    $this->post('', VariacaoProdutoController::class . ':criar'); // POST /variacaoProduto
-    $this->put('/{id}', VariacaoProdutoController::class . ':atualizar');  // PUT /variacaoProduto/{id}
-    $this->delete('/{id}', VariacaoProdutoController::class . ':deletar'); // DELETE /variacaoProduto/{id}
-});
+return function (App $app) {
+    $app->group('/variacaoProduto', function (RouteCollectorProxy $group) {
+        $group->get('', [VariacaoProdutoController::class, 'listar']);
+        $group->get('/{id}', [VariacaoProdutoController::class, 'buscarPorId']);
+        $group->post('', [VariacaoProdutoController::class, 'criar']);
+        $group->put('/{id}', [VariacaoProdutoController::class, 'atualizar']);
+        $group->delete('/{id}', [VariacaoProdutoController::class, 'deletar']);
+    });
+};
