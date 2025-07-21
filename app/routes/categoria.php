e5d95c85d6edabd968fd0cce3638479b27293f9e
FlameOff <jgmppacheco@gmail.com>
2025-07-21 14:28:31 -0300

    fix:categoria.php

--- app/routes/categoria.php
+++ app/routes/categoria.php
@@ -1,13 +1,14 @@
 <?php
 
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 use App\Controller\CategoriaController;
 
 return function (App $app) {
-    $app->group('/categoria', function ($group) {
+    $app->group('/categoria', function (RouteCollectorProxy $group) {
         $group->get('', [CategoriaController::class, 'listar']);
         $group->get('/{id}', [CategoriaController::class, 'buscarPorId']);
-        $group->post('', [CategoriaController::class, 'inserir']);
+        $group->post('', [CategoriaController::class, 'criar']);
         $group->put('/{id}', [CategoriaController::class, 'atualizar']);
         $group->delete('/{id}', [CategoriaController::class, 'deletar']);
     });
