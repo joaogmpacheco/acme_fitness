f680cc8ac77774d5ffefe0088453df4cc951f2fe
FlameOff <jgmppacheco@gmail.com>
2025-07-21 15:40:36 -0300

    fix:dependencies

--- app/dependencies.php
+++ app/dependencies.php
@@ -2,7 +2,42 @@
 
 declare(strict_types=1);
 
+use function DI\autowire;
+
 use App\Application\Settings\SettingsInterface;
+use App\Controller\{
+    CategoriaController,
+    ClienteController,
+    ConfiguracaoSistemaController,
+    EnderecoController,
+    ItemVendaController,
+    ProdutoController,
+    VariacaoProdutoController,
+    VendaController
+};
+
+use App\Service\{
+    CategoriaService,
+    ClienteService,
+    ConfiguracaoSistemaService,
+    EnderecoService,
+    ItemVendaService,
+    ProdutoService,
+    VariacaoProdutoService,
+    VendaService
+};
+
+use App\DAO\{
+    CategoriaDAO,
+    ClienteDAO,
+    ConfiguracaoSistemaDAO,
+    EnderecoDAO,
+    ItemVendaDAO,
+    ProdutoDAO,
+    VariacaoProdutoDAO,
+    VendaDAO
+};
+
 use DI\ContainerBuilder;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -10,56 +45,62 @@ use Monolog\Processor\UidProcessor;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 
-// DAO
-use App\DAO\CategoriaDAO;
-use App\DAO\ClienteDAO;
-use App\DAO\ConfiguracaoSistemaDAO;
-use App\DAO\EnderecoDAO;
-use App\DAO\ItemVendaDAO;
-use App\DAO\ProdutoDAO;
-use App\DAO\VariacaoProdutoDAO;
-use App\DAO\VendaDAO;
-
-// Service
-use App\Service\CategoriaService;
-use App\Service\ClienteService;
-use App\Service\ConfiguracaoSistemaService;
-use App\Service\EnderecoService;
-use App\Service\ItemVendaService;
-use App\Service\ProdutoService;
-use App\Service\VariacaoProdutoService;
-use App\Service\VendaService;
-
-return function (ContainerBuilder $containerBuilder): void {
+return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
 
+        // Logger
         LoggerInterface::class => function (ContainerInterface $c) {
             $settings = $c->get(SettingsInterface::class);
             $loggerSettings = $settings->get('logger');
             $logger = new Logger($loggerSettings['name']);
+
             $logger->pushProcessor(new UidProcessor());
-            $logger->pushHandler(new StreamHandler($loggerSettings['path'], $loggerSettings['level']));
+            $logger->pushHandler(new StreamHandler(
+                $loggerSettings['path'],
+                $loggerSettings['level']
+            ));
+
             return $logger;
         },
 
-        // DAO bindings
-        CategoriaDAO::class => fn(ContainerInterface $c) => new CategoriaDAO(),
-        ClienteDAO::class => fn(ContainerInterface $c) => new ClienteDAO(),
-        ConfiguracaoSistemaDAO::class => fn(ContainerInterface $c) => new ConfiguracaoSistemaDAO(),
-        EnderecoDAO::class => fn(ContainerInterface $c) => new EnderecoDAO(),
-        ItemVendaDAO::class => fn(ContainerInterface $c) => new ItemVendaDAO(),
-        ProdutoDAO::class => fn(ContainerInterface $c) => new ProdutoDAO(),
-        VariacaoProdutoDAO::class => fn(ContainerInterface $c) => new VariacaoProdutoDAO(),
-        VendaDAO::class => fn(ContainerInterface $c) => new VendaDAO(),
+        // PDO connection
+        PDO::class => function () {
+            return new PDO(
+                'mysql:host=localhost;dbname=acme;charset=utf8mb4',
+                'root',
+                '', // senha
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
+        },
 
-        // Service bindings
-        CategoriaService::class => fn(ContainerInterface $c) => new CategoriaService($c->get(CategoriaDAO::class)),
-        ClienteService::class => fn(ContainerInterface $c) => new ClienteService($c->get(ClienteDAO::class)),
-        ConfiguracaoSistemaService::class => fn(ContainerInterface $c) => new ConfiguracaoSistemaService($c->get(ConfiguracaoSistemaDAO::class)),
-        EnderecoService::class => fn(ContainerInterface $c) => new EnderecoService($c->get(EnderecoDAO::class)),
-        ItemVendaService::class => fn(ContainerInterface $c) => new ItemVendaService($c->get(ItemVendaDAO::class)),
-        ProdutoService::class => fn(ContainerInterface $c) => new ProdutoService($c->get(ProdutoDAO::class)),
-        VariacaoProdutoService::class => fn(ContainerInterface $c) => new VariacaoProdutoService($c->get(VariacaoProdutoDAO::class)),
-        VendaService::class => fn(ContainerInterface $c) => new VendaService($c->get(VendaDAO::class)),
+        // DAOs (autowire já injeta o PDO)
+        CategoriaDAO::class => autowire(CategoriaDAO::class),
+        ClienteDAO::class => autowire(ClienteDAO::class),
+        ConfiguracaoSistemaDAO::class => autowire(ConfiguracaoSistemaDAO::class),
+        EnderecoDAO::class => autowire(EnderecoDAO::class),
+        ItemVendaDAO::class => autowire(ItemVendaDAO::class),
+        ProdutoDAO::class => autowire(ProdutoDAO::class),
+        VariacaoProdutoDAO::class => autowire(VariacaoProdutoDAO::class),
+        VendaDAO::class => autowire(VendaDAO::class),
+
+        // Services
+        CategoriaService::class => autowire(CategoriaService::class),
+        ClienteService::class => autowire(ClienteService::class),
+        ConfiguracaoSistemaService::class => autowire(ConfiguracaoSistemaService::class),
+        EnderecoService::class => autowire(EnderecoService::class),
+        ItemVendaService::class => autowire(ItemVendaService::class),
+        ProdutoService::class => autowire(ProdutoService::class),
+        VariacaoProdutoService::class => autowire(VariacaoProdutoService::class),
+        VendaService::class => autowire(VendaService::class),
+
+        // Controllers
+        CategoriaController::class => autowire(CategoriaController::class),
+        ClienteController::class => autowire(ClienteController::class),
+        ConfiguracaoSistemaController::class => autowire(ConfiguracaoSistemaController::class),
+        EnderecoController::class => autowire(EnderecoController::class),
+        ItemVendaController::class => autowire(ItemVendaController::class),
+        ProdutoController::class => autowire(ProdutoController::class),
+        VariacaoProdutoController::class => autowire(VariacaoProdutoController::class),
+        VendaController::class => autowire(VendaController::class),
     ]);
 };
