872ac68abee732daa54836f6f34ebab98bbc3ea9
FlameOff <jgmppacheco@gmail.com>
2025-07-21 13:09:48 -0300

    fix:ProdutoController

--- app/Controller/ProdutoController.php
+++ app/Controller/ProdutoController.php
@@ -2,24 +2,27 @@
 
 namespace App\Controller;
 
-use App\DAO\ProdutoDAO;
-use App\Model\Produto;
+use App\Service\ProdutoService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class ProdutoController {
 
+    private ProdutoService $produtoService;
+
+    public function __construct(ProdutoService $produtoService) {
+        $this->produtoService = $produtoService;
+    }
+
     public function listar(Request $request, Response $response): Response {
-        $dao = new ProdutoDAO();
-        $produtos = $dao->listar();
-        $response->getBody()->write(json_encode($produtos));
+        $produto = $this->produtoService->listar();
+        $response->getBody()->write(json_encode($produto));
         return $response->withHeader('Content-Type', 'application/json');
     }
 
     public function buscarPorId(Request $request, Response $response, array $args): Response {
         $id = (int) $args['id'];
-        $dao = new ProdutoDAO();
-        $produto = $dao->listarPorId($id);
+        $produto = $this->produtoService->listarPorId($id);
 
         if (!$produto) {
             $response->getBody()->write(json_encode(['erro' => 'Produto não encontrado']));
@@ -32,10 +35,7 @@ class ProdutoController {
 
     public function criar(Request $request, Response $response): Response {
         $data = $request->getParsedBody();
-        $produto = new Produto($data);
-
-        $dao = new ProdutoDAO();
-        $sucesso = $dao->inserir($produto);
+        $sucesso = $this->produtoService->criar($data);
 
         $status = $sucesso ? 201 : 500;
         $mensagem = $sucesso ? ['mensagem' => 'Produto criado com sucesso'] : ['erro' => 'Erro ao criar produto'];
@@ -48,11 +48,7 @@ class ProdutoController {
         $id = (int) $args['id'];
         $data = $request->getParsedBody();
 
-        $produto = new Produto($data);
-        $produto->setId($id);
-
-        $dao = new ProdutoDAO();
-        $sucesso = $dao->atualizar($produto);
+        $sucesso = $this->produtoService->atualizar($id, $data);
 
         $mensagem = $sucesso ? ['mensagem' => 'Produto atualizado com sucesso'] : ['erro' => 'Erro ao atualizar produto'];
         $status = $sucesso ? 200 : 500;
@@ -63,11 +59,9 @@ class ProdutoController {
 
     public function deletar(Request $request, Response $response, array $args): Response {
         $id = (int) $args['id'];
+        $sucesso = $this->produtoService->deletar($id);
 
-        $dao = new ProdutoDAO();
-        $sucesso = $dao->deletar($id);
-
-        $mensagem = $sucesso ? ['mensagem' => 'Produto deletado com sucesso'] : ['erro' => 'Erro ao deletar produto'];
+        $mensagem = $sucesso ? ['mensagem' => 'Endereco deletado com sucesso'] : ['erro' => 'Erro ao deletar produto'];
         $status = $sucesso ? 200 : 500;
 
         $response->getBody()->write(json_encode($mensagem));
