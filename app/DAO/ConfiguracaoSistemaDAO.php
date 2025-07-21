dbd91d7f7df79ee4f603aadafcb9675462f52964
FlameOff <jgmppacheco@gmail.com>
2025-07-21 17:23:56 -0300

    refactor:app/DAO/ConfiguracaoSistemaDAO.php

--- app/DAO/ConfiguracaoSistemaDAO.php
+++ app/DAO/ConfiguracaoSistemaDAO.php
@@ -1,15 +1,14 @@
 <?php
 
 namespace App\DAO;
-use App\Database\Connection;
 use App\Model\ConfiguracaoSistema;
 use PDO;
 
 class ConfiguracaoSistemaDAO {
     private PDO $pdo;
 
-    public function __construct() {
-        $this->pdo = Connection::get();
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
     }
 
     public function listar(): array {
