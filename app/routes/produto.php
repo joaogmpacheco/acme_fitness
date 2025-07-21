c56577c8aa9c8f944b199ff3855fe5d0f6951701
FlameOff <jgmppacheco@gmail.com>
2025-07-21 17:22:11 -0300

    refactor:app/routes/produto.php

--- app/routes/produto.php
+++ app/routes/produto.php
@@ -1,14 +1,16 @@
-<?php 
+<?php
 
+use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 use App\Controller\ProdutoController;
 
-$app->group('/produto', function () {
-    $this->get('', ProdutoController::class . ':listar');         // GET /produto
-    $this->get('/{id}', ProdutoController::class . ':listarPorId'); // GET /produto/{id}
-    $this->post('', ProdutoController::class . ':criar');          // POST /produto
-    $this->put('/{id}', ProdutoController::class . ':atualizar');  // PUT /produto/{id}
-    $this->delete('/{id}', ProdutoController::class . ':deletar'); // DELETE /produto/{id}
-});
+return function (App $app) {
+    $app->group('/produto', function (RouteCollectorProxy $group) {
+        $group->get('', [ProdutoController::class, 'listar']);
+        $group->get('/{id}', [ProdutoController::class, 'buscarPorId']);
+        $group->post('', [ProdutoController::class, 'criar']);
+        $group->put('/{id}', [ProdutoController::class, 'atualizar']);
+        $group->delete('/{id}', [ProdutoController::class, 'deletar']);
+    });
+};
 
-
-?>
